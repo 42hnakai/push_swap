f85b49e53e9fd59b5b1f0bd40f37c678f4a7a7b5
hnakai <hnakai@student.42tokyo.jp>
2023-06-24 19:05:02 +0900

    add headerfile and testmain

---
+++ ft_push_swap.h
@@ -0,0 +1,22 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <string.h>
+
+//push_swapフォルダ下にあるファイル内の関数を宣言
+void	ft_sa(char *ary_a);
+void	ft_sb(char *ary_b);
+void	ft_ss(char *ary_a, char *ary_b);
+void	ft_pa(char *ary_a, char *ary_b);
+void	ft_pb(char *ary_a, char *ary_b);
+void	ft_ra(char *ary_a);
+void	ft_rb(char *ary_b);
+void	ft_rr(char *ary_a, char *ary_b);
+void	ft_rra(char *ary_a);
+void	ft_rrb(char *ary_b);
+void	ft_rrr(char *ary_a, char *ary_b);
+void	ft_swap(char *x, char *y);
+size_t	ft_strlen(const char *str);
+
+#endif
